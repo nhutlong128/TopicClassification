8da1001840ca744f8f537b76efd028eb6bc5c63f
Long Huynh <nhutlong128@gmail.com>
2020-11-29 21:36:51 +0700

    SVC Predict with argument

--- src/model/svc_predict.py
+++ src/model/svc_predict.py
@@ -2,21 +2,40 @@ from pathlib import Path
 import sklearn
 import numpy as np
 import pickle
-
-
+import argparse
+import sys
+sys.path.append('src/data/')
+from processing_data import get_processing_data
+from sklearn.datasets import load_files
+import os.path
 
 
 if __name__ == '__main__':
-    document = 'tate lyle bos bag top award r tate lyle chief executive ha been named european businessman of the year by leading business magazine r iain ferguson wa awarded the title by u publication forbes for returning one of the uk venerable manufacturer to the country top 100 company the sugar group had been absent from the ftse 100 for seven year until mr ferguson helped it return to growth tate share have leapt 55 this year boosted by firming sugar price and sale of it artificial sweetener r after year of sagging stock price and seven year hiatus from the ftse 100 one of britain venerable manufacturer ha returned to the vaunted index forbes said mr ferguson took the helm at the company in 2003 after spending most of his career at consumer good giant unilever tate lyle which wa an original member of the historic ft 30 index in 1935 operates more than 41 factory and 20 more additional production facility in 28 country previous winner of the forbes award include royal bank of scotland'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", '--DocumentFolder', type=str, required=True, help="Folder of Input Documents")
+    args = parser.parse_args()
+    path = Path(__file__).parent / f"../../"
+    folder_data = load_files(path, categories=args.DocumentFolder)
+    data = folder_data.data
+    if not data:
+        print('Empty Folder')
+        exit()
+    document = get_processing_data(data)
+
     tfidf_path = Path(__file__).parent / "../../data/features/tfidf.pickle"
     with open(tfidf_path, 'rb') as f:
         tfidf = pickle.load(f)
-    doc_vector = tfidf.transform([document]).toarray()
+    doc_vector = tfidf.transform(document).toarray()
     
     svc_path = Path(__file__).parent / "../../model/SVC.pickle"
     with open(svc_path, 'rb') as f:
         svc = pickle.load(f)
     pred = svc.predict(doc_vector)
     category_dict = {0:"Business", 1: 'Entertainment', 2:'Politics', 3:'Sport', 4:'Tech'}
+    results = [category_dict[x] for x in pred]
     
-    print(category_dict[pred[0]])
+    file_name = [os.path.split(x)[-1] for x in folder_data.filenames]
+    prediction = zip(file_name, results)
+    prediction = list(prediction)
+    
+    print(prediction)
